57410968f2d04b28c6f40c1cc8e90d33f0720178
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-04-11 10:20:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,18 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+from PIL import Image
+
 
 #import pickle5 as pickle
 #from pathlib import Path
 
 st.title('Chatea con tu PDF 💬')
-ke = st.text_input('Ingresa tu Clave')
+ke = st.text_input('Ingresa tu clave para comenzar')
+
+image = Image.open('chatconpdf.png')
+st.image(image, width=200)
+
 #os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 os.environ['OPENAI_API_KEY'] = ke
 
@@ -44,7 +50,7 @@ if pdf is not None:
       knowledge_base = FAISS.from_texts(chunks, embeddings)
 
 # show user input
-      st.subheader("Escribe que quieres saber sobre el documento")
+      st.subheader("Escribe qué quieres saber sobre el documento")
       user_question = st.text_input(" ")
       if user_question:
         docs = knowledge_base.similarity_search(user_question)
